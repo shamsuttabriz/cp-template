22985abf0b38407ca66e6ed210d5df37b0f17aa7
shamsuttabriz <shamsuttabriz.md@gmail.com>
2026-03-31 20:09:27 +0600

    Add initial project files including main program, input/output handling, and build tasks

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+    {
+        cout << "Hello world!" << endl;
+    }
+
+    return 0;
+}
---
+++ template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+
+    return 0;
+}
